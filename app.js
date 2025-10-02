c9ae4d3578aa27e9433aa3461dada8b5b81c08a5
hanna-ngx2308 <hoghah2308@gmail.com>
2025-10-02 11:16:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 [
-  { "label": "Undergraduate", "value": 93624, "color": "#4da3ff" },
-  { "label": "Master's", "value": 33860, "color": "#7dd3fc" },
-  { "label": "Doctorate", "value": 18294, "color": "#60a5fa" },
-  { "label": "Korean Language Program", "value": 48924, "color": "#a78bfa" },
-  { "label": "Others/Exchange/Training", "value": 114260, "color": "#fbbf24" }
+  {
+    "Country": "China",
+    "Students_2024": 72020,
+    "Percentage": 34.5
+  },
+  {
+    "Country": "Vietnam",
+    "Students_2024": 56003,
+    "Percentage": 26.8
+  },
+  {
+    "Country": "Mongolia",
+    "Students_2024": 12317,
+    "Percentage": 5.9
+  },
+  {
+    "Country": "Uzbekistan",
+    "Students_2024": 12025,
+    "Percentage": 5.8
+  },
+  {
+    "Country": "Nepal",
+    "Students_2024": 6135,
+    "Percentage": 2.9
+  },
+  {
+    "Country": "Myanmar",
+    "Students_2024": 5000,
+    "Percentage": 2.7
+  },
+  {
+    "Country": "Others",
+    "Students_2024": 37500,
+    "Percentage": 21.4
+  }
 ]
